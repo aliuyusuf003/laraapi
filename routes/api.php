89a0a2e6e24857edb7cd7d22d6729d19a69b9b0b
Aliu Yusuf <aliu.yusuf@outlook.com>
2021-06-08 10:07:13 +0100

    Integrate Login and Register Routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
  
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,16 +17,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/products',ProductController::class);
+// Note: 
+// You need to register to get your token
+// You also get a unique token when you login
+// You need to use your token to create,update,delete a product
+
+// Route::resource('/products',ProductController::class);
 // Route::get('/products/search/{name}',[ProductController::class, 'search']);
 
 
-// Route::get('/products',[ProductController::class, 'index']);
-// Route::post('/products',[ProductController::class, 'store']);
+// Public Routes 
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
+Route::get('/products',[ProductController::class, 'index']);
+Route::get('/products/{id}',[ProductController::class, 'show']);
+Route::get('/products/search/{name}',[ProductController::class, 'search']);
+
+
+// Protected Routes 
 // Route::middleware('auth:sanctum')->get('/products/search/{name}',[ProductController::class, 'search']);
    
 Route::group(['middleware' => 'auth:sanctum'],function(){
-    Route::get('/products/search/{name}',[ProductController::class, 'search']);
+    Route::post('/products',[ProductController::class, 'store']);
+    Route::put('/products/{id}',[ProductController::class, 'update']);
+    Route::delete('/products/{id}',[ProductController::class, 'destroy']);
+    Route::post('/logout',[AuthController::class, 'logout']);
+
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
